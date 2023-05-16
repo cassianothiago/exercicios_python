0ccf25d77a8a1e6540aa9fa087e2d48b15736c4d
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:18:34 -0400

    Create produto-metade-primeiro.py

---
+++ produto-metade-primeiro.py
@@ -0,0 +1,9 @@
+a=float(input('digite o primeiro numero: '))
+b=float(input('digite o segundo numero: '))
+c=float(input('digite o terceiro numero: '))
+prod=(2*a)*(b/2)
+soma=(3*a)+c
+cubo=(c**3)
+print(prod)
+print(soma)
+print(cubo)
