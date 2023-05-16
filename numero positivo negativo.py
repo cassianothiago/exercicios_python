41c327fd053158cfbc0e60a206542303407cd980
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:18:26 -0400

    Create numero positivo negativo.py

---
+++ numero positivo negativo.py
@@ -0,0 +1,11 @@
+print('numero positivo, negativo ou neutro')
+print('='*120)
+a=int(input('digite um numero: '))
+if a>0:
+    print('o numero %d e positivo '%a)
+elif a<0:
+    print('o numero %d e negativo '%a)
+else:
+    print('o numero 0 é neutro')
+print('='*120)
+
