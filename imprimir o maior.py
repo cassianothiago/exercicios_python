3c537e0deaf3796cda5670b38f26ff23d1167e84
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:17:55 -0400

    Create imprimir o maior.py

---
+++ imprimir o maior.py
@@ -0,0 +1,11 @@
+print('qual é o maior número? ')
+print('='*120)
+a=int(input('digite o primeiro numero: '))
+b=int(input('digite o segundo numero: '))
+if a>b:
+    print(a)
+elif a==b:
+    print('são iguais')
+else:
+    print(b)
+print('='*120)
