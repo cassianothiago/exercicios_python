f7801fb7a2a0f01d7561d900bc96642c22ddb54e
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:17:48 -0400

    Create horas_trabalhada.py

---
+++ horas_trabalhada.py
@@ -0,0 +1,4 @@
+horas=float(input('Digite quantas horas vc trabalhou no mês: '))
+valor_hora=float(input('Digite quanto vale a sua hora: '))
+horas_trabalho=horas*valor_hora
+print('seu salario total no mês é: R$ %.4f'%horas_trabalho)
