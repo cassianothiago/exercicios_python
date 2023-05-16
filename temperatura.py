2a631c2539bc506e09735a993a34f8290d0429c8
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:19:11 -0400

    Create temperatura.py

---
+++ temperatura.py
@@ -0,0 +1,6 @@
+print(50*"-")
+print("conversao de temperatura \n")
+temperatura=float(input("digite a temperatura em fahrenheit: "))
+celsus=float(5/9)*(temperatura-32)
+
+print("o valor em celsus é: %.3f"%celsus)
