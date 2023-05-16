8310fb32cca77f2eac5ea04cd2e788903396239d
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:19:14 -0400

    Create teoria-listas.py

---
+++ teoria-listas.py
@@ -0,0 +1,34 @@
+a=[10,20,30,40,50]
+b=['python',60,3.5,True,1]
+c=['oi',2.0,5,[10,20]]
+d=[]
+e=[[]]
+f=[17,123,87,34,66,8398,44]
+g=[3,67,'gato',[56,57,'cachorro'],[],3,14,False]
+h=['gato','cachorro']
+i=['maca','laramja','banana','cereja']
+
+print('='*120)
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+print(type(c))
+print(c)
+print(type(d))
+print(d)
+print(len(a))
+print(len(b))
+print(len(c))
+print(len(d))
+print(len(e))
+print(f[0])
+print(f[-2])
+print(f[5])
+print(f[0:4])
+print(g[2][0])
+print(g[3][1])
+print(g[3][2][0])
+print('gato'in h)
+print('arara'not in h)
+print(57 in g)
