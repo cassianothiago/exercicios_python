0b324d5fa24b3b7056e9d2783b0941f99e0fdfbf
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:17:16 -0400

    Create concatenar valores.py

---
+++ concatenar valores.py
@@ -0,0 +1,6 @@
+a=100
+b=200.5
+c="ederson"
+
+print("o valor de a é %d, e o valor de %.2f, e o nome é %s"%(a,b,c))#identificando variavel pelo tipo (mais recomendado)
+print("o valor de a é ",a,"valor de b é ",b,"e o nome é ",c)#identificando variavel por virgula
