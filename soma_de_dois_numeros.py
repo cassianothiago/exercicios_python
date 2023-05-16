a0142edb04a80d07e6d4eb51a7d006bfa63c2188
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:19:05 -0400

    Create soma_de_dois_numeros.py

---
+++ soma_de_dois_numeros.py
@@ -0,0 +1,9 @@
+import math
+a=int(input('digite o primeiro numero: '))
+b=int(input('digite o segundo numero: '))
+s=a+b
+print('a soma dos numeros: {}'.format(s))
+raiz=math.sqrt(s)
+exp=math.exp(s)
+print(raiz,'\n',exp)
+
