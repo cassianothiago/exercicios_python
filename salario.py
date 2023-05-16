f31dac2d7cc256e411e08070dba085c00311c0b7
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:18:55 -0400

    Create salario.py

---
+++ salario.py
@@ -0,0 +1,12 @@
+salario=int(input('salario?= '))
+imposto=float(input('digite o salario= '))
+
+x=str(input('salario?= '))
+z=float(input('salario?= '))
+y=bool(input('salario?='))
+
+print(salario)
+print(imposto)
+print(x)
+print(z)
+print(y)
