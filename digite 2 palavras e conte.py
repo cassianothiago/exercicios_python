52ecd0b6ed813f7a75dae8ffaccc0f577d8c29fd
cassiano <cassianothiago@rocketmail.com>
2023-05-16 08:17:32 -0400

    Create digite 2 palavras e conte.py

---
+++ digite 2 palavras e conte.py
@@ -0,0 +1,3 @@
+a=input('digite a primeira palavra: ')
+b=input('digite a segunda palavra: ')
+print('a primeira palavra é %s e a segunda é %s e o total dos caracteres respectivamente é %s e %s '%(a,b,len(a),len(b)))
