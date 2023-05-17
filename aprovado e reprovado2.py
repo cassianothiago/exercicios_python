404c10957ba41cc6bad4eb8a5fddb5eff4505e06
cassianothiago <cassianothiago@rocketmail.com>
2023-05-17 16:05:10 -0300

    Create aprovado e reprovado2.py

---
+++ aprovado e reprovado2.py
@@ -0,0 +1,27 @@
+print('='*120)
+a=float(input('digite a primeira nota: '))
+b=float(input('digite a segunda nota: '))
+media=(a+b)/2
+
+if media>=7:
+    print('Aprovado')
+
+else:
+    print('Reprovado')
+print('='*120)
+
+if media==10:
+    print('Distinção')
+
+print('='*120)
+a=float(input('digite a primeira nota: '))
+b=float(input('digite a segunda nota: '))
+media=(a+b)/2
+
+if media>=7:
+    print('Aprovado')
+    if media==10:
+        print('Distinção')
+else:
+    print('Reprovado')
+print('='*120)
