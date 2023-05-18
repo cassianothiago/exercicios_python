e9a263139428c2ae0ce3f8793bd3a3c246549f1e
cassianothiago <cassianothiago@rocketmail.com>
2023-05-18 14:51:55 -0300

    Create exerc_9.py

---
+++ lista de exercicios python/exerc_9.py
@@ -0,0 +1,8 @@
+#Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+print('='*120)
+cont=0
+while cont<50:
+    cont=cont+1
+    print(cont%2!=0,end=', ')
+print('='*120)
+
