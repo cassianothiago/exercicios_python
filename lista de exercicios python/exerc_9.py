d8693d75ec65a68a6358fb27a7d9e0f02262a28e
cassiano <cassianothiago@rocketmail.com>
2023-05-18 20:37:00 -0400

    update

--- lista de exercicios python/exerc_9.py
+++ lista de exercicios python/exerc_9.py
@@ -1,8 +1,10 @@
 #Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 print('='*120)
+
 cont=0
 while cont<50:
     cont=cont+1
-    print(cont/2!=0,end=(' ,'))
+    if cont%2!=0:
+        print(cont,end=(', '))
 print('')
 print('='*120)
--- lista de exercicios python/teste3.py
+++
@@ -1,4 +0,0 @@
-sexo=input('digite m ou f: ')
-while (sexo!= 'f') and (sexo!='m'):
-    sexo = input("Biologicamente, você deve ser 'f' ou 'm': ")
-print(sexo)
