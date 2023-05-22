e614976eb58ea32feabd578d7f540a8fc7aff73c
cassianothiago <cassianothiago@rocketmail.com>
2023-05-22 18:01:11 -0300

    update

--- lista de exercicios python/exerc_22.py
+++ lista de exercicios python/exerc_22.py
@@ -1,14 +1,17 @@
 #Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+
 num=int(input('digite um numero: '))
 cont=0
-while cont<=num:
+cont2=0
+while cont<=num or cont<2:
     cont=+1
     div=num%cont
     if div==0:
-        print(div)
-        
-        break
+        cont2=cont2+1
+
+    if cont<=2:
+        print('primo')
     else:
         print('não é primo')
-        break
+    
 
