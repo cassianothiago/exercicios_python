d8798fea9c28f62cc870d1b417fdf780b3ba6628
cassianothiago <cassianothiago@rocketmail.com>
2023-05-24 18:27:52 -0300

    Update for numeros inteiros.py

--- for numeros inteiros.py
+++ for numeros inteiros.py
@@ -1,5 +1,7 @@
+print('='*120)
+print('intervalo entre os numeros')
 a=int(input('digite o 1º numero: '))
 b=int(input('digite o 2º numero: '))
 for x in range(a,b):
     print(x)
-    
+print('='*120)
