5d9c3738ee33bd07ce89f4a889bfd50de5488cb4
cassianothiago <cassianothiago@rocketmail.com>
2023-05-25 16:07:13 -0300

    Update exerc_28.py

--- lista de exercicios python/exerc_28.py
+++ lista de exercicios python/exerc_28.py
@@ -1,2 +1,15 @@
 #Faça um programa que calcule o número médio de alunos por turma. Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma. As turmas não podem ter mais de 40 alunos.
 
+import os
+turmas=int(input('digite quantidade de turmas: '))
+cont=0
+lista=[]
+while cont<turmas:
+    cont=cont+1
+    print('cadastro da turma ',cont)
+    alunos=int(input('digite a quantidade de alunos da turma: '))
+    if alunos>40:
+        print('numero de alunos por turma não pode ser maior que 40')
+    continue
+
+    
