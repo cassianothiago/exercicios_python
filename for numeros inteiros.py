bcb84c63f31301ee6b747c7efb719a177df79361
cassianothiago <cassianothiago@rocketmail.com>
2023-05-26 15:05:19 -0300

    Update for numeros inteiros.py

--- for numeros inteiros.py
+++ for numeros inteiros.py
@@ -2,6 +2,10 @@ print('='*120)
 print('intervalo entre os numeros')
 a=int(input('digite o 1º numero: '))
 b=int(input('digite o 2º numero: '))
+soma=0
+print('os intervalos entre {} e {} são: '.format(a,b))
 for x in range(a,b):
     print(x)
+    soma=soma+x
+print('e a soma dos intervalos = {}'.format(soma))
 print('='*120)
