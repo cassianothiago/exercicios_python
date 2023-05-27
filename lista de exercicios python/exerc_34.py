a9e85c1a613618498fab543e12a756c0a6a87624
cassiano <cassianothiago@rocketmail.com>
2023-05-27 18:37:14 -0400

    Create exerc_34.py

---
+++ lista de exercicios python/exerc_34.py
@@ -0,0 +1,25 @@
+'''34.	O Departamento Estadual de Meteorologia lhe contratou para desenvolver um programa 
+que leia as um conjunto indeterminado de temperaturas, 
+e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.'''
+
+import os
+print('='*120)
+lista=[]
+soma=0
+cont=0
+while True:
+    temp=(input('digite as temperaturas e digite "X" para sair: '))
+    cont=cont+1
+    if temp=='x' or temp=='X':
+        break
+    temp=float(temp)
+    lista.append(temp)
+    soma=soma+temp
+    media=soma/cont
+if lista!=0:
+    print('a menor temperatura= ',min(lista))
+    print('a maior temperaturra= ',max(lista))
+    print('soma de totas as temperaturas digitadas= ',sum(lista))
+    print('a media das temperaturas= ',media)
+print('='*120)
+os.system('pause')
