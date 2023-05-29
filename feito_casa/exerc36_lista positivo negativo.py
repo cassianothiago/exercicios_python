d7629031a2787cd7c41a12f22c737674f9b9b4cd
cassianothiago <cassianothiago@rocketmail.com>
2023-05-29 18:30:52 -0300

    Update exerc36_lista positivo negativo.py

--- feito_casa/exerc36_lista positivo negativo.py
+++ feito_casa/exerc36_lista positivo negativo.py
@@ -1,9 +1,16 @@
 #Crie uma lista vazia, que será usado para receber 10 números com a estrutura FOR apresente ao final o total de números negativos e positivos digitados pelo usuário.
 
 lista=[]
+cont=0
+cont2=0
 for i in range(4):
-    lista.append(int(input('digite o numero: ')))
-    print(lista[0])
-
-
+    x=int(input('digite um nomero: '))
+    lista.append(x)
+    if x<0:
+        cont=cont+1
+    if x>0:
+        cont2=cont2+1
+print(cont)
+print(cont2)
+print(lista)
 
