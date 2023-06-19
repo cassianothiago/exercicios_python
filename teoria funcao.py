8e61b4bfebfee3324f3c6d4774a0e64746ad0d8b
cassiano <132009888+cassianothiago@users.noreply.github.com>
2023-06-19 15:23:35 -0400

    Add files via upload

--- teoria funcao.py
+++ teoria funcao.py
@@ -3,11 +3,40 @@
 
 hello()'''
 
-def hello2(nome):
+'''def hello2(nome):
     print('olá',nome)
 
 
 
 
 a = input('digite: ')
-hello2('thiago')
+hello2('thiago')'''
+
+'''def hello3(nome,idade):
+    print('olá',nome,'\nsua idade é :',idade)
+
+hello3('miguel',7)'''
+
+'''def calcular_pag(quant_hora,valor_hora):
+    horas=float(quant_hora)
+    taxa=float(valor_hora)
+    if horas <=40:
+        salario=horas*taxa
+    else:
+        h_exced=horas-40
+        salario=40*taxa+(h_exced*(1.5*taxa))
+    print(salario)
+
+calcular_pag(30,60)
+calcular_pag(50,60)'''
+
+def soma(x,y):
+    result=x+y
+    return result
+
+a=int(input('digite o primeiro numero: '))
+b=int(input('digite o segundo numero: '))
+soma(a,b)
+res=soma(a,b)
+print('soma = ',res)
+
