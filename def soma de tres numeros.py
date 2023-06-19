5263842a7d7f0c30ec6c80e764c3d8485b78a451
cassiano <132009888+cassianothiago@users.noreply.github.com>
2023-06-19 17:03:40 -0400

    Add files via upload

--- def soma de tres numeros.py
+++ def soma de tres numeros.py
@@ -1,4 +1,4 @@
-def soma():
+'''def soma():
     a=int(input('digite o primeiro numero: '))
     b=int(input('digite o segundo numero: '))
     c=int(input('digite o terceiro numero: '))
@@ -6,4 +6,14 @@ def soma():
     return somatoria
 print('   soma de tres numeros   ')
 somado=soma()
+print(somado)'''
+
+def soma2(x,y,z):
+    somatoria=x+y+z
+    return somatoria
+print('   soma de tres numeros   ')
+a=int(input('digite o primeiro numero: '))
+b=int(input('digite o segundo numero: '))
+c=int(input('digite o terceiro numero: '))
+somado=soma2(a,b,c)
 print(somado)
