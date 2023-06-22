7d5fafc334b52f78250c2ed17d4479222f3c34eb
cassiano <cassianothiago@rocketmail.com>
2023-06-22 13:59:55 -0400

    Update def valor pagamento.py

--- def valor pagamento.py
+++ def valor pagamento.py
@@ -1,3 +1,21 @@
 
-prest=float(input('digite o valor da prestacao: '))
-dias=int(input('digite quantos dias está atrasado: '))
+def valorpagamento(valor,dias):
+    pagamento=valor+(valor*0.03+dias*0.001)
+    return pagamento
+
+
+print('caixa')
+listprest=[]
+listdias=[]
+listpag=[]
+prest=float(input('Digite o valor da prestação: '))
+listprest.append(prest)
+atraso=int(input('Digite os dias de atraso ou zero se estiver em dia: '))
+if atraso>0:
+    listdias.append(atraso)
+    x=valorpagamento(prest,atraso)
+    listpag.append(x)
+print(listprest)
+print(listdias)
+print(listpag)
+
