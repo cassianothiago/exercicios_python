593b0990032dee104a98ec9e333fdb8bb30c67b0
cassiano <cassianothiago@rocketmail.com>
2023-06-25 18:45:30 -0400

    Update def valor pagamento2.py

--- def valor pagamento2.py
+++ def valor pagamento2.py
@@ -3,17 +3,35 @@
 listacliente=[]
 listaprest=[]
 listdias=[]
-cont=0
 while True:
     cliente=input('digite o nome do cliente ou S para sair: ')
     if cliente=='s' or cliente=='S':
         break
     listacliente.append(cliente)
     prest=float(input('digite o valor da conta: '))
-    listaprest.append(prest)
     dias=int(input('digite quantos dias está em atraso: '))
     listdias.append(dias)
-for i in range(len(listacliente)):
-    print('cliente:',listacliente[i],',prestação:',listaprest[i],'atraso:',listdias[i])
+    if dias>0:
+        pagamento=prest+(prest*0.03+dias*0.001*prest)
+        prest=pagamento
+        listaprest.append(prest)
+    elif dias==0:
+        listaprest.append(prest)
     
+for i in range(len(listacliente)):
+        print('-----')
+        print('cliente:',listacliente[i],'\nprestação:',listaprest[i],'\natraso:',listdias[i])
+print('-----')
+print('total dos cliente:',sum(listaprest))
+while True:
+     flog=input('imprimir o flog do cliente digite seu nome ou E para encerrar: ')
+     if flog=='E' or flog=='e':
+          break
+     else:
+        a=listacliente.index(flog)
+        print('-----')
+        print('cliente:',listacliente[a],'\nprestação:',listaprest[a],'\natraso',listdias[a])
+
+
+             
     
