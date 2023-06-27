2ef981b164bd578a00b7be2255b1fa7f482ccea7
Gabriel Gravena Barros <cassianothiago@rocketmail.com>
2023-06-27 16:59:14 -0300

    Update aula pandas.py

--- aula pandas.py
+++ aula pandas.py
@@ -15,7 +15,7 @@ tabela_vendas=pd.read_excel("Vendas.xlsx")
 #produtos=tabela_vendas[['Produto','ID Loja']]
 #print(produtos.head())
 #print(tabela_vendas.loc[1:5])
-print(tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping'])
+#print(tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping'])
 x=tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping']
 print(x.head())
 
