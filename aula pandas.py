35d91934b0f96ba808ee9141f571314cc398b61b
cassiano <cassianothiago@rocketmail.com>
2023-06-27 21:23:56 -0400

    upgrade

--- aula pandas.py
+++ aula pandas.py
@@ -1,22 +1,25 @@
 import pandas as pd
 #dataframe=pd.DataFrame()
-'''print('-----')
-venda={'data':['26/06/2023','27/06/2023'],'valor':[15,20],'produto':['feijão','arroz'],'qtde':[50,70]}
+#print(dataframe)
+'''venda={'data':['26/06/2023','27/06/2023'],'valor':[15,20],'produto':['feijão','arroz'],'qtde':[50,70]}
 print(venda)
 tabela_vendas=pd.DataFrame(venda)
-print('-----')
-print(tabela_vendas)
-print('-----')'''
+print(tabela_vendas)'''
 tabela_vendas=pd.read_excel("Vendas.xlsx")
 #print(tabela_vendas)
-#print(tabela_vendas.head(10))
+#print(tabela_vendas.head())
 #print(tabela_vendas.shape)
 #print(tabela_vendas.describe())
 #produtos=tabela_vendas[['Produto','ID Loja']]
-#print(produtos.head())
-#print(tabela_vendas.loc[1:5])
+#print(produtos)
+#print(tabela_vendas.loc[1:2])
 #print(tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping'])
-x=tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping']
-print(x.head())
+#vendas_NorteShopping=tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping']
+#print(vendas_NorteShopping.head(1))
+vendas_NorteShopping=tabela_vendas.loc[tabela_vendas['ID Loja']=='Norte Shopping',['Produto','Quantidade']]
+print(vendas_NorteShopping)
+print(tabela_vendas.loc[4,'Produto'])
+
+
 
 
