1342a7f2c57e84bd6c8300bda096023f12b1cdc7
Gabriel Gravena Barros <cassianothiago@rocketmail.com>
2023-08-01 18:15:00 -0300

    Update pyside6.py

--- orientação_ao_objeto/pyside6.py
+++ orientação_ao_objeto/pyside6.py
@@ -1,5 +1,29 @@
-from PySide6.QtWidgets import QAapplication, QWidget
+import typing
+import PySide2.QtCore
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLabel
+from PySide2.QtCore import QSize, Qt
 import sys
-app=QAapplication(sys.argv)
-janela=QWidget()
+
+'''class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Hello World!!!")
+		button = QPushButton("Jho soy um butão")
+		self.setCentralWidget(button)
+		button.clicked.connect(self.imprimir)
+	def imprimir(self):
+		print("Professor Maurício")'''
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__(self)
+        self.setWindowTitle('Meu Programa')
+        self.setFixedSize(600,400)
+        
+        
+  
+  
+app = QApplication(sys.argv)
+janela = MainWindow()
 janela.show()
+app.exec_()
